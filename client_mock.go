e4526998da1f9b3b25cc5143e8e8f80209b16353
nickolashkraus <0x@nickolaskraus.io>
2023-01-05 16:02:46 -0600

    Update copyright

--- client_mock.go
+++ client_mock.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Infrable. All rights reserved.
+// Copyright 2023 Infrable. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
